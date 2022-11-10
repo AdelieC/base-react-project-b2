407bf99f178907ad7de15f66809368ed749ada04
AdelieC <castel.adelie@yahoo.com>
2022-11-10 10:36:53 +0100

    morning work

---
+++ src/pages/Disclaimer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Disclaimer = () => {
+	return (
+		<main className="text-white">
+			<h1 className="animate-ping">Mentions légales</h1>
+		</main>
+	);
+};
+
+export default Disclaimer;
--- src/utils/routes/pages-index.js
+++ src/utils/routes/pages-index.js
@@ -1,7 +1,16 @@
 import HomePage from "../../pages/HomePage";
 import AboutPage from "../../pages/AboutPage";
+import Disclaimer from "../../pages/Disclaimer";
 
 export const PAGES = {
 	HomePage,
 	AboutPage,
+	Disclaimer,
 };
+
+const array = ["a", "b", "c"];
+const object = { a: "at", b: "bt", c: "ct" };
+
+const b = array[1];
+
+const a = object[a];
